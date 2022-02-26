6d2b09b76e79763da9590819894f1ab2acac0b61
Chun-gu <chungu.yi92@gmail.com>
2022-02-26 23:56:04 +0900

    ✨ Buttons 컴포넌트 Home 페이지에 임시로 렌더

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,42 @@
+import { Buttons } from '../components';
+
 function Home() {
-  return <div>Home</div>;
+  const onSale = 3;
+  return (
+    <>
+      <Buttons.L disabled={false}>버튼</Buttons.L>
+      <Buttons.L disabled>버튼</Buttons.L>
+
+      <Buttons.M color="green" disabled={false}>
+        버튼
+      </Buttons.M>
+      <Buttons.M color="green" disabled>
+        버튼
+      </Buttons.M>
+      <Buttons.M color="dark" disabled>
+        버튼
+      </Buttons.M>
+      <Buttons.M color="white" disabled>
+        버튼
+      </Buttons.M>
+
+      <Buttons.MS>버튼</Buttons.MS>
+      <Buttons.MS>버튼</Buttons.MS>
+      <Buttons.MS>버튼</Buttons.MS>
+      <Buttons.MS>버튼</Buttons.MS>
+
+      <Buttons.S>버튼</Buttons.S>
+      <Buttons.S>버튼</Buttons.S>
+      <Buttons.S>버튼</Buttons.S>
+
+      <Buttons.Tab isActive>버튼</Buttons.Tab>
+      <Buttons.Tab isActive>버튼</Buttons.Tab>
+
+      <Buttons.Menu isActive>판매중인 상품({onSale})</Buttons.Menu>
+      <Buttons.Menu isActive>판매중인 상품</Buttons.Menu>
+      <Buttons.Menu isActive>판매중인 상품</Buttons.Menu>
+    </>
+  );
 }
 
 export default Home;
