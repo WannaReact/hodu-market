e35628814527d6d093061efc2a3beb0c01020b6e
Chun-gu <chungu.yi92@gmail.com>
2022-02-27 23:53:24 +0900

    🏷️ Buttons 컴포넌트 타입 부여

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -1,49 +1,106 @@
-import { ReactNode } from 'react';
-import { S_L, S_M, S_MS, S_S, S_TAB, S_MENU } from './styled';
+import { MouseEventHandler, ReactNode } from 'react';
+import { S_L, S_M, S_MS, S_S, S_TAB, S_MENU, S_BADGE } from './styled';
 
-type IButtonProps = {
+interface IButtonProps {
   children: ReactNode;
-};
-
-type ILButtonProps = IButtonProps & {
-  disabled: boolean;
-};
-
-type IMButtonProps = IButtonProps & {
-  disabled: boolean;
-  color: 'green' | 'dark' | 'white';
-};
-type ITabButtonProps = IButtonProps & {
-  isActive: boolean;
-};
-type IMenuButtonProps = IButtonProps & {
-  isActive: boolean;
-};
-
-export function L({ disabled, children }: ILButtonProps) {
-  return <S_L disabled={disabled}>{children}</S_L>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export function M({ disabled, color, children }: IMButtonProps) {
+interface ILButtonProps extends IButtonProps {
+  disabled: boolean;
+}
+
+interface IMButtonProps extends IButtonProps {
+  disabled: boolean;
+  color: 'green' | 'dark' | 'white';
+}
+
+interface IMSButtonProps extends IButtonProps {
+  color: 'green' | 'white';
+}
+
+interface ISButtonProps extends IButtonProps {
+  color: 'green' | 'white';
+}
+
+interface ITabButtonProps extends IButtonProps {
+  isActive: boolean;
+}
+
+interface IMenuButtonProps extends IButtonProps {
+  isActive: boolean;
+  badgeCount: number | undefined;
+}
+
+export function L({ onClick, disabled, children }: ILButtonProps) {
   return (
-    <S_M disabled={disabled} color={color}>
+    <S_L onClick={onClick} disabled={disabled}>
+      {children}
+    </S_L>
+  );
+}
+
+export function M({ onClick, disabled, color, children }: IMButtonProps) {
+  return (
+    <S_M onClick={onClick} disabled={disabled} color={color}>
       {children}
     </S_M>
   );
 }
 
-export function MS({ children }: IButtonProps) {
-  return <S_MS>{children}</S_MS>;
+export function MS({ onClick, color, children }: IMSButtonProps) {
+  return (
+    <S_MS onClick={onClick} color={color}>
+      {children}
+    </S_MS>
+  );
 }
 
-export function S({ children }: IButtonProps) {
-  return <S_S>{children}</S_S>;
+export function S({ onClick, color, children }: ISButtonProps) {
+  return (
+    <S_S onClick={onClick} color={color}>
+      {children}
+    </S_S>
+  );
 }
 
-export function Tab({ isActive, children }: ITabButtonProps) {
-  return <S_TAB {...isActive}>{children}</S_TAB>;
+export function Tab({ onClick, isActive, children }: ITabButtonProps) {
+  return (
+    <S_TAB onClick={onClick} isActive={isActive}>
+      {children}
+    </S_TAB>
+  );
 }
 
-export function Menu({ isActive, children }: IMenuButtonProps) {
-  return <S_MENU {...isActive}>{children}</S_MENU>;
+export function Menu({
+  onClick,
+  isActive,
+  badgeCount,
+  children
+}: IMenuButtonProps) {
+  return (
+    <S_MENU onClick={onClick} isActive={isActive}>
+      {children}
+      {!!badgeCount && <S_BADGE>{badgeCount}</S_BADGE>}
+    </S_MENU>
+  );
 }
+
+L.defaultProps = {
+  onClick: () => {}
+};
+M.defaultProps = {
+  onClick: () => {}
+};
+MS.defaultProps = {
+  onClick: () => {}
+};
+S.defaultProps = {
+  onClick: () => {}
+};
+Tab.defaultProps = {
+  onClick: () => {}
+};
+Menu.defaultProps = {
+  onClick: () => {}
+};
