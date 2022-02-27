67e78181cf29db046697d87e0e107db7dde3a710
Chun-gu <chungu.yi92@gmail.com>
2022-02-27 23:52:56 +0900

    💄 스타일 작성 및 색상코드 상수로 변경

--- components/Buttons/styled.ts
+++ components/Buttons/styled.ts
@@ -1,53 +1,136 @@
 import styled from 'styled-components';
+import { COLOR } from '../../shared/constants';
 
 export const S_L = styled.button`
   width: 22rem;
   height: 6.8rem;
-  color: #ffffff;
+  color: ${COLOR.white};
   font-size: 2.4rem;
   font-weight: 700;
-  background-color: ${(props) => (props.disabled ? '#c4c4c4' : '#21bf48')};
+  background-color: ${(props) =>
+    props.disabled ? COLOR.greyC4 : COLOR.accentColor};
   border-radius: 0.5rem;
 `;
 
 export const S_M = styled.button`
   width: 48rem;
   height: 6rem;
-  color: ${(props) => (props.color === 'white' ? '#767676' : '#ffffff')};
-  font-size: 18px;
+  color: ${(props) => (props.color === 'white' ? COLOR.grey76 : COLOR.white)};
+  font-size: 1.8rem;
   font-weight: 700;
-  background-color: ${(props) => (props.disabled ? '#c4c4c4' : '#21bf48')};
+  background-color: ${(props) => (props.disabled ? COLOR.greyC4 : '')};
+  ${(props) => {
+    switch (props.color) {
+      case 'green':
+        return `background-color: ${COLOR.accentColor}`;
+      case 'dark':
+        return `background-color: ${COLOR.grey76}`;
+      case 'white':
+        return `
+        background-color: ${COLOR.white};
+        border: 0.1rem solid ${COLOR.greyC4}`;
+      default:
+        return ``;
+    }
+  }};
+  ${(props) => (props.disabled ? `background-color: ${COLOR.greyC4}` : '')};
   border-radius: 0.5rem;
 `;
 
 export const S_MS = styled.button`
   width: 16.8rem;
   height: 5.4rem;
-  color: #ffffff;
-  font-size: 18px;
+  font-size: 1.6rem;
   font-weight: 500;
-  background-color: ${(props) => (props.disabled ? '#c4c4c4' : '#21bf48')};
+  ${(props) => {
+    switch (props.color) {
+      case 'green':
+        return `
+        color: ${COLOR.white}; 
+        background-color: ${COLOR.accentColor}`;
+      case 'white':
+        return `
+        color: ${COLOR.grey76}; 
+        background-color: ${COLOR.white}; 
+        border: 0.1rem solid ${COLOR.greyC4}; 
+        &:hover {
+          color:${COLOR.black}; 
+          border-color: ${COLOR.grey76};
+        }`;
+      default:
+        return '';
+    }
+  }};
+  padding: 0 2rem;
   border-radius: 0.5rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const S_S = styled.button`
   width: 8rem;
   height: 4rem;
-  background-color: #21bf48;
+  ${(props) => {
+    switch (props.color) {
+      case 'green':
+        return `
+        color: ${COLOR.white}; 
+        background-color: ${COLOR.accentColor}`;
+      case 'white':
+        return `
+        color: ${COLOR.grey76}; 
+        background-color: ${COLOR.white}; 
+        border: 0.1rem solid ${COLOR.greyC4}; 
+        &:hover {
+          color:${COLOR.black}; 
+          border-color: ${COLOR.grey76};
+        }`;
+      default:
+        return '';
+    }
+  }};
   border-radius: 0.5rem;
 `;
 
-export const S_TAB = styled.button`
+export const S_TAB = styled.button<{ isActive: boolean }>`
   width: 32rem;
   height: 6rem;
-  color: #767676;
-  background-color: #ffffff;
-  border: 6px soild #21bf48;
+  color: ${(props) => (props.isActive ? COLOR.accentColor : COLOR.grey76)};
+  font-size: 1.8rem;
+  font-weight: ${(props) => (props.isActive ? 700 : 500)};
+  background-color: ${COLOR.white};
+  border-bottom: 0.6rem solid;
+  border-color: ${(props) =>
+    props.isActive ? COLOR.accentColor : COLOR.greyE0};
 `;
 
-export const S_MENU = styled.button`
+export const S_MENU = styled.button<{ isActive: boolean }>`
   width: 25rem;
   height: 5rem;
-  color: #000000;
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: ${(props) => (props.isActive ? COLOR.white : COLOR.black)};
+  background-color: ${(props) =>
+    props.isActive ? COLOR.accentColor : COLOR.white};
   border-radius: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  &:hover {
+    background-color: ${(props) =>
+      props.isActive ? COLOR.accentColor : COLOR.greyE0};
+  }
+`;
+
+export const S_BADGE = styled.span`
+  height: 2rem;
+  color: ${COLOR.white};
+  font-size: 1.2rem;
+  font-weight: 500;
+  line-height: 2rem;
+  background-color: ${COLOR.red};
+  border-radius: 1rem;
+  padding: 0 0.7rem;
 `;
