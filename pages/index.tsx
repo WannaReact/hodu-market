78d2a81023d878e04255ed909197ceedaebcfa12
skgml0 <hnh2714@gmail.com>
2022-03-02 21:54:10 +0900

    ✨ 메인페이지 아이템 UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,23 @@
-import Image from 'next/image';
-import { Buttons } from '../components';
-import plusIcon from '../public/images/icon-plus.png';
+import { nanoid } from 'nanoid';
+import styled from 'styled-components';
+import ProductCard from '../components/ProductCard';
+import dummyData from './api/dummyData.json';
+
+const CardsContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 380px);
+`;
 
 function Home() {
-  const onClick = () => {};
   return (
-    <>
-      <Buttons.L disabled={false}>버튼</Buttons.L>
-      <Buttons.L disabled>버튼</Buttons.L>
-      <br />
-      <Buttons.M color="green" disabled={false}>
-        버튼
-      </Buttons.M>
-      <Buttons.M color="green" disabled>
-        버튼
-      </Buttons.M>
-      <br />
-      <Buttons.M color="dark" disabled={false} onClick={onClick}>
-        버튼
-      </Buttons.M>
-      <Buttons.M color="white" disabled={false}>
-        버튼
-      </Buttons.M>
-      <br />
-      <Buttons.MS color="green">
-        <Image src={plusIcon} width={32} height={32} />
-        상품 업로드
-      </Buttons.MS>
-      <Buttons.MS color="green">버튼</Buttons.MS>
-      <Buttons.MS color="white">버튼</Buttons.MS>
-      <Buttons.MS color="white">버튼</Buttons.MS>
-      <br />
-      <Buttons.S color="green">버튼</Buttons.S>
-      <Buttons.S color="white">버튼</Buttons.S>
-      <Buttons.S color="white">버튼</Buttons.S>
-      <br />
-      <Buttons.Tab isActive>버튼</Buttons.Tab>
-      <Buttons.Tab isActive={false}>버튼</Buttons.Tab>
-      <br />
-      <Buttons.Menu isActive={false} badgeCount={0}>
-        판매중인 상품
-      </Buttons.Menu>
-      <Buttons.Menu isActive={false} badgeCount={3}>
-        판매중인 상품
-      </Buttons.Menu>
-      <Buttons.Menu isActive badgeCount={100}>
-        판매중인 상품
-      </Buttons.Menu>
-    </>
+    <main>
+      <article>슬라이드</article>
+      <CardsContainer>
+        {dummyData.map((data) => (
+          <ProductCard {...data} key={nanoid()} />
+        ))}
+      </CardsContainer>
+    </main>
   );
 }
 
