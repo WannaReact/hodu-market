b89f65001716c8e971fb48549bb382499ed69186
정민킴 <jma1020@naver.com>
2022-03-03 21:21:39 +0900

    :sparkles: 판매자 page 리뷰 컴포넌트 생성

---
+++ components/seller/Reviews.tsx
@@ -0,0 +1,27 @@
+import * as Styled from './styled';
+import ReviewTr from './ReviewTr';
+
+function Reviews() {
+  const data: number[] = [1, 2, 3, 4];
+
+  return (
+    <table style={{ width: '100%' }}>
+      <thead>
+        <Styled.Tr className="test">
+          <Styled.Headtd flex={30}>상품정보</Styled.Headtd>
+          <Styled.Headtd flex={10}>작성자</Styled.Headtd>
+          <Styled.Headtd flex={40}>문의 내용</Styled.Headtd>
+          <Styled.Headtd flex={10}>처리 상태</Styled.Headtd>
+          <Styled.Headtd flex={10}>쓰기/편집</Styled.Headtd>
+        </Styled.Tr>
+      </thead>
+      <Styled.TbodyTest>
+        {data.map((i) => {
+          return <ReviewTr key={i} />;
+        })}
+      </Styled.TbodyTest>
+    </table>
+  );
+}
+
+export default Reviews;
