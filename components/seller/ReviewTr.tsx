5852136c151d7c04e6fea38c3429e39d0ce5d0af
정민킴 <jma1020@naver.com>
2022-03-03 22:59:52 +0900

    :adhesive_bandage: styled 파일 ProfileCheck 타입변경 및 UI 및 태그 변경

--- components/seller/ReviewTr.tsx
+++ components/seller/ReviewTr.tsx
@@ -5,7 +5,7 @@ import * as Styled from './styled';
 import profileImg from '../../public/images/seller-productIMG.png';
 
 interface ProfileCheck {
-  profile?: number;
+  profile?: boolean;
   flex: number;
   isText?: boolean;
   isAlign?: boolean;
@@ -49,20 +49,24 @@ function ReviewTr() {
   return (
     <>
       <Styled.Tr onClick={answer} className="test">
-        <Styled.Bodytd profile={1} flex={30}>
+        <Styled.Bodytd hasProfile flex={30}>
           <Image src={profileImg} width={120} height={120} />
           <Styled.ProductText>
             <h4>
-              안녕 상품정보를 너무 많이 넣으면 코로나 걸려버려 그러고 열 40도
-              즉사 ㅂㅇ 그리고 여긴 상품 타이틀이야 길게 적을 필요없어
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint eum
+              dolore earum commodi consectetur odit. Delectus, ea pariatur, rem
+              beatae totam a architecto laborum cupiditate perspiciatis
+              reprehenderit sapiente quo. Dolor?
             </h4>
             <p>안녕</p>
           </Styled.ProductText>
         </Styled.Bodytd>
         <Styled.Bodytd flex={10}>jma1020</Styled.Bodytd>
         <Styled.Bodytd flex={40}>
-          대충문의 호로록 대충문의 대충문의 호로록 대충문의 대충문의 호로록
-          대충문의 대충문의 호로록 대충문의 대충문의 호로록 대충문의
+          <p>2022.02.15</p>
+          <p>별점</p>
+          <p>리뷰내용리뷰내용</p>
+          <Image src={profileImg} width={120} height={120} />
         </Styled.Bodytd>
         <Styled.Bodytd flex={10}>답변완료</Styled.Bodytd>
         <Styled.Bodytd flex={10}>버튼 </Styled.Bodytd>
@@ -78,9 +82,9 @@ function ReviewTr() {
                   <em>2022.02.15</em>
                   <p>
                     저는 정민입니다 대충 문의 답변 드릴게요?? 그냥 써주세요.
-                    저는 정민입니다 대충 문의 답변 드릴게요?? 그냥 써주세요.
-                    저는 정민입니다 대충 문의 답변 드릴게요?? 그냥 써주세요.
-                    저는 정민입니다 대충 문의 답변 드릴게요?? 그냥 써주세요
+                    저는 정민입니다 대충 문의 답변 드 써주세요. 저는 정민입니다
+                    대충 문의 답변 드릴게요?? 그냥 써주릴게요?? 그냥 써주세요.
+                    저는 정민입니다 대충 문의 답변 드릴게요?? 그냥세요
                   </p>
                 </CommentTd>
                 <CommentTd flex={10} isAlign>
