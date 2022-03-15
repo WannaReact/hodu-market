26444fe44fc80180ae56dd288deb544c677d1426
Chun-gu <chungu.yi92@gmail.com>
2022-03-15 23:46:57 +0900

    💄 product detail 페이지 스타일 추가 작성

--- components/ReviewItem/styled.ts
+++ components/ReviewItem/styled.ts
@@ -11,14 +11,25 @@ export const ReviewContainer = styled.li`
 export const ReviewInfo = styled.div`
   width: 14.5rem;
   max-height: 8rem;
+  display: flex;
+  & > div:first-child {
+    margin-right: 1rem;
+  }
+`;
+
+export const RateWrapper = styled.div`
+  margin-bottom: 0.5rem;
 `;
 
 export const Author = styled.span`
+  display: block;
   font-size: 1.8rem;
   color: ${COLOR.black};
+  margin-bottom: 0.4rem;
 `;
 
 export const Date = styled.span`
+  display: block;
   font-size: 1.6rem;
   color: ${COLOR.grey76};
 `;
