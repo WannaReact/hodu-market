9032684fb9e0f73d330b6920a547d59936b3f7a1
Tesseract <tensai3355@gmail.com>
2022-03-18 01:33:09 +0900

    :recycle: 불필요한 코드 정리

--- lib/mongoose/dbConnect.ts
+++ lib/mongoose/dbConnect.ts
@@ -1,26 +1,16 @@
 import mongoose from 'mongoose';
 
-interface Cache {
-  conn: object | null;
-  promise: object | null;
-}
-
 const { MONGODB_URI } = process.env;
 
 if (!MONGODB_URI) {
   throw new Error('환경 변수 MONGODB_URI 값이 올바르지 않습니다!');
 }
 
-const cache: Cache = { conn: null, promise: null };
+let cache: typeof mongoose | null = null;
 
 export default async () => {
-  if (cache.conn) {
-    return cache.conn;
+  if (!cache) {
+    cache = await mongoose.connect(MONGODB_URI);
   }
-  if (!cache.promise) {
-    cache.promise = await mongoose.connect(MONGODB_URI);
-  }
-  cache.conn = await cache.promise;
-  console.log('connected to database');
-  return cache.conn;
+  return cache;
 };
