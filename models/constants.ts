6dcfa7f04aec93256b79479e7d6b3d1c2604a258
Tesseract <tensai3355@gmail.com>
2022-03-20 23:33:51 +0900

    :bug: 잘못된 정규표현식 수정

--- models/constants.ts
+++ models/constants.ts
@@ -1,2 +1,2 @@
 export const IMAGE_MATCH = /\.(jpg|jpeg|png|gif)$/;
-export const DISCOUNT_MATCH = /(^[1-9][0-9]{,8}$)|(^[1-9][0-9]?%$)/;
+export const DISCOUNT_MATCH = /(^[1-9][0-9]{0,8}$)|(^[1-9][0-9]?%$)/;
