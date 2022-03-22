beac4b4e72f1bceb2b86afb57e79c8b791076333
skgml0 <hnh2714@gmail.com>
2022-03-22 20:37:03 +0900

    💄 유효성 안내 텍스트 UI 추가

--- components/Inputs/styled.ts
+++ components/Inputs/styled.ts
@@ -42,10 +42,21 @@ export const Label = styled.label`
 
 export const Limit = styled.p`
   position: absolute;
-  bottom: 1.8rem;
-  right: 1.6rem;
+  bottom: 50%;
+  transform: translateY(100%);
+  right: 2.6rem;
   font-weight: 300;
   font-size: 1.6rem;
+  color: ${COLOR.greyC4};
+`;
+export const ValidationMsg = styled.p`
+  position: absolute;
+  top: 50%;
+  /* transform: translateY(100%); */
+  right: 1.6rem;
+  font-weight: 300;
+  font-size: 1rem;
+  vertical-align: top;
   color: ${COLOR.greyC4};
 `;
 
