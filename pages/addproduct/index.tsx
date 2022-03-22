593c6fb08a0e3e83e14a261d1d3fb54e7361b4eb
정민킴 <jma1020@naver.com>
2022-03-22 09:39:34 +0900

    :sparkles: 상품등록 페이지 이미지 업로드 기능 추가

--- pages/addproduct/index.tsx
+++ pages/addproduct/index.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+// import styled from 'styled-components';
+
 function AddproductPage() {
-  return <>안녕</>;
+  const [createObjectURL, setCreateObjectURL] = useState<any>(null);
+
+  const uploadToClient = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+
+      setCreateObjectURL(URL.createObjectURL(i));
+    }
+  };
+
+  return (
+    <>
+      <header>상품 등록</header>
+      <main>
+        <img src={createObjectURL} alt="임시" />
+        <input type="file" id="img" onChange={uploadToClient} />
+      </main>
+    </>
+  );
 }
+
+// const Image = styled.img`
+// `
 
 export default AddproductPage;
