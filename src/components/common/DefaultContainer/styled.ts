5db0f52f55609ab5ed4ed018f516cb61eb96c80f
Tesseract <tensai3355@gmail.com>
2022-04-17 13:03:15 +0900

    :adhesive_bandage: NAV_WIDTH 값 변경 및 좌우 패딩 상수로 변경

--- src/components/common/DefaultContainer/styled.ts
+++ src/components/common/DefaultContainer/styled.ts
@@ -1,7 +1,8 @@
-import { NAV_HEIGHT, NAV_WIDTH } from '@shared/constants';
+import { MIN_MARGIN, NAV_HEIGHT, NAV_WIDTH } from '@shared/constants';
 import styled from 'styled-components';
 
 export const DefaultContainer = styled.div`
-  max-width: ${NAV_WIDTH}rem;
+  max-width: ${NAV_WIDTH + MIN_MARGIN * 2}rem;
+  padding: 0 ${MIN_MARGIN}rem;
   margin: ${NAV_HEIGHT}rem auto 0;
 `;
