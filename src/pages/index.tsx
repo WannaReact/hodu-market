34d539e566a621b48c28b0867349c8fcfe9962ce
skgml0 <hnh2714@gmail.com>
2022-04-25 00:20:01 +0900

    💄 main페이지 grid 정렬 UI구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,29 @@
-// import { GetStaticProps } from 'next';
 import { nanoid } from 'nanoid';
+import DefaultContainerPage from 'src/components/common/DefaultContainer';
 import styled from 'styled-components';
 import ProductCard from '../components/ProductCard';
 import dummyData from './api/dummyData.json';
 
-const CardsContainer = styled.section`
-  display: grid;
-  grid-template-columns: repeat(3, 380rem);
-`;
-
 function Home() {
   return (
-    <main>
+    <>
       <article>슬라이드</article>
-      <CardsContainer>
-        {dummyData.map((data) => (
-          <ProductCard {...data} key={nanoid()} />
-        ))}
-      </CardsContainer>
-    </main>
+      <DefaultContainerPage>
+        <CardsContainer>
+          {dummyData.map((data) => (
+            <ProductCard {...data} key={nanoid()} />
+          ))}
+        </CardsContainer>
+      </DefaultContainerPage>
+    </>
   );
 }
 
-// export const getStaticProps: GetStaticProps = () => {
-//   return {
-//     props: {
-//       gnb: {
-//         active: true,
-//         title: '관리자 페이지'
-//       }
-//     }
-//   };
-// };
-
 export default Home;
+
+const CardsContainer = styled.section`
+  display: grid;
+  align-content: space-around;
+  grid-template-columns: repeat(3, minmax(380px, auto));
+  gap: 0.8rem 0;
+`;
