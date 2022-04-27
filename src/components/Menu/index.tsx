171a21ab648f3f2812b19d112edc5eba266bc611
Tesseract <tensai3355@gmail.com>
2022-04-27 17:27:55 +0900

    :adhesive_bandage: badge 추가

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,16 +8,20 @@ import * as Styled from './styled';
 interface MenuProps {
   menu: string | string[] | undefined;
   menuText: { [key: string]: string };
+  badge: { [key: string]: number } | null;
 }
 
-function Menu({ menu, menuText }: MenuProps) {
+function Menu({ menu, menuText, badge }: MenuProps) {
   const { pathname } = useRouter();
   return (
     <Styled.NavBar>
       {Object.keys(menuText).map((key) => (
         <Link key={nanoid()} href={pathname.replace('[menu]', key)} passHref>
           <li>
-            <Buttons.Menu isActive={menu === key} badgeCount={0}>
+            <Buttons.Menu
+              isActive={menu === key}
+              badgeCount={badge?.[key] ?? 0}
+            >
               {menuText[key]}
             </Buttons.Menu>
           </li>
