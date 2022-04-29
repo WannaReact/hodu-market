ae85cbf6a56c56ebb42831ffb94403a73fd87c6f
Chun-gu <chungu.yi92@gmail.com>
2022-04-29 21:34:34 +0900

    🔥 불필요한 코드 삭제

--- src/components/CommentItem/index.tsx
+++ src/components/CommentItem/index.tsx
@@ -1,35 +1,22 @@
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import api from '@utils/api';
 import ImageWrapper from '@utils/ImageWrapper';
-import { IComment, IUser } from '@shared/types';
+import { IComment } from '@shared/types';
 import { dateConverter } from '@utils/dateConverter';
 import authorImg from 'public/images/product-img-small-1.png';
 import * as Styled from './styled';
 
 export function CommentItem({
-  userId,
-  // authorImg,
+  user: { nickname },
   content,
   createdAt
 }: IComment) {
-  const [userData, setUserData] = useState<IUser>();
-  const getData = async () => {
-    const { data } = await api.get(`/user/${userId}`);
-    setUserData(data);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
     <Styled.CommentItem>
       <Styled.Author>
         <ImageWrapper width="4rem" height="4rem" imgStyle="border-radius: 50%;">
           <Image src={authorImg} layout="fill" />
         </ImageWrapper>
-        <span>{userData?.userName}</span>
+        <span>{nickname}</span>
       </Styled.Author>
       <Styled.Content>{content}</Styled.Content>
       <Styled.Date>{dateConverter(createdAt)}</Styled.Date>
